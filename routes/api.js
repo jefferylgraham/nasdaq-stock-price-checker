88274d5053b2143e6f16e0f9fafc1c655d8baa47
Glitch (jeffgraham-nasdaq-stock-price-checker) <none>
2018-11-29 02:18:28 +0000

    add security via helmet

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,17 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+var db;
 
 module.exports = function (app) {
+  
+  MongoClient.connect(CONNECTION_STRING, function(err, database) {
+      if(err) {
+        console.log(err);
+      }
+      console.log('Connected...');
+      db = database;
+  });
 
   app.route('/api/stock-prices')
     .get(function (req, res){
